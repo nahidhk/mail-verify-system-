72733d429baf486d8636210a3c4e1ca38a5851b2
nahidappltd@gmail.com <nahidappltd@gmail.com>
2024-01-05 09:16:45 +0600

    new

---
+++ v/v.js
@@ -0,0 +1,8 @@
+function servr(){
+    document.getElementById("icon").classList="a2 bi bi-check-circle"
+  }
+  function call(){
+    window.location.href="https://www.mailr.bmhm.sbs/user?verified"
+  }
+  setTimeout(servr,1300);
+    setTimeout(call,1700)
